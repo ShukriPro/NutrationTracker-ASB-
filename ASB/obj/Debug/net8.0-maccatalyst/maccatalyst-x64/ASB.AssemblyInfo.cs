4830a251708ab7684f71ac66b77643308629dc95
ShukriPro <justshukri078@gmail.com>
2024-06-23 12:54:27 +1200

    Your commit message here

--- ASB/obj/Debug/net8.0-maccatalyst/maccatalyst-x64/ASB.AssemblyInfo.cs
+++ ASB/obj/Debug/net8.0-maccatalyst/maccatalyst-x64/ASB.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ASB")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+7965faa3391d053d73a3f5a3467fd0ccd9f97133")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+5392a552265703bf44668ef81f9fb19c80ae9f6f")]
 [assembly: System.Reflection.AssemblyProductAttribute("ASB")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ASB")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
